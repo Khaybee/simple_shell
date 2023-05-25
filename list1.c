9ff2226f2f884bf1b8dc9d82f4e389c5ae985788
Anki Gilbert OKosso <ankigilbertokosso@gmail.com>
2023-05-25 07:58:26 -0700

    update

--- list1.c
+++ list1.c
@@ -15,15 +15,10 @@ var_list *add_var_node(var_list **head, int len_var, char *val, int len_val)
 	new = malloc(sizeof(var_list));
 	if (new == NULL)
 		return (NULL);
-	new->var_len = len_var;
-	new->value = NULL;
-	new->len_value = 0;
 
-	if (val != NULL && len_val > 0)
-	{
-		new->value = _strdup(val);
-		new->len_value = len_val;
-	}
+	new->var_len = len_var;
+	new->value = val;
+	new->len_value = len_val;
 
 	new->next = NULL;
 	temp = *head;
